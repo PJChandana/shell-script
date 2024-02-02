90c33301de23597b676cf9c496712d968d50ed09
PJChandana <PJChandana@gmail.com>
2024-02-02 20:07:37 +0530

    scripting

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -2,10 +2,10 @@
 
 FRUITS=("APPLE" "MANGO" "POMG")
 
-echo "Firts value: ${FRUITS(0)}"
+echo "Firts value: ${FRUIT[(0]}"
 
-echo "Second value: ${FRUITS(1)}"
+echo "Second value: ${FRUITS[1]}"
 
-echo "Thirds value: ${FRUITS(2)}"
+echo "Thirds value: ${FRUITS[2]}"
 
-echo "ALL FRUITS: ${FRUITS(@)}"
+echo "All fruits: ${FRUITS[@]}"
