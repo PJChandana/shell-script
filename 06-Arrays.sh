b52cd939b34088952c259a31e83d686d8c52974a
PJChandana <PJChandana@gmail.com>
2024-02-02 20:05:14 +0530

    scripting

---
+++ 06-Arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "MANGO" "POMG")
+
+echo "Firts value: ${FRUITS(O)}"
+
+echo "Second value: ${FRUITS(1)}"
+
+echo "Thirds value: ${FRUITS(2)}"
+
+echo "ALL FRUITS: ${FRUITS(@)}"
