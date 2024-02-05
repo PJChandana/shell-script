6b78e3ff1eb9a69bfec1422d90061dd00166c9af
PJChandana <PJChandana@gmail.com>
2024-02-05 20:13:01 +0530

    scripting

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ID=$(id-u)
+R="\e[31m"
+Y="\e[32m"
+N="\e[0m"
 
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR: run this script with root access $N"
     exit 1
 else
-    echo "You are roort user"
+    echo "You are root user"
 fi
 
 echo "All args passed: $@"
