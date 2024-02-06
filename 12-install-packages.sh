cf0e3805f514e89c07394d8278ca72d6f7a02165
PJChandana <PJChandana@gmail.com>
2024-02-06 19:51:54 +0530

    scripting

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -2,8 +2,20 @@
 
 ID=$(id -u)
 R="\e[31m"
-Y="\e[32m"
+G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date+%F+%H+%S)
+LOGFILE="/tmp/$0-TIMESTAMP.log" 
+
+VALIDATE(){
+    if[ $1 -ne 0 ]
+    then 
+        echo -e "$2... $R FAILED $N"
+    else
+        echo -e "$2... $G SUCCESS $N"
+}
 
 if [ $ID -ne 0 ]
 then 
@@ -13,5 +25,19 @@ else
     echo "You are root user"
 fi
 
-echo "All args passed: $@"
+#echo "All args passed: $@"
+
+for package in $@
+do
+    yum list installed $package
+    if [ $?-ne 0 ] 
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+    else
+        echo -e "$package is already installed.. $Y SKIPPING $N"
+    fi
+done
+
+
 
