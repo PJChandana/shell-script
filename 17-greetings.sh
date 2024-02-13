f7e1c54101c9f06526732465b184a756f146f708
PJChandana <PJChandana@gmail.com>
2024-02-13 19:45:28 +0530

    shellscripts

--- 17-greetings.sh
+++ 17-greetings.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "Hello$PERSON,Goodmorning. we are learining shell script"
+USAGE(){
+    echo "USAGE::$(basename $0) -n <name> - w <wishes>"
+    echo "options::"
+    echo " -n, specify the name (mandatory)"
+    echo " -w, Specify the wishes. ex, Good morning"
+    echo " -h  Dispaly Help and Exit"
+}
+
+while getopts "n:w:h" opt; do
+    case $opt in
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
+        :))USAGE; exit;;
+        h|*) USAGE; exit;;
+       
+    esac
+done
