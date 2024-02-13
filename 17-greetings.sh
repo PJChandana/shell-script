cbfa93ffc06182336e62e804984b544eafb74df5
PJChandana <PJChandana@gmail.com>
2024-02-13 21:34:10 +0530

    shellscripts

--- 17-greetings.sh
+++ 17-greetings.sh
@@ -22,7 +22,7 @@ while getopts "n:w:h" opt; do
 done
 
 if [ -z"NAME" ] || [-z "WISHES" ]; then
-    echo "Error: Both -n and-w are manatory options."
+    echo "Error: Both -n and-w are mandatory options."
     USAGE
     exit 1
 fi
